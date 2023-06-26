2d1be6709d189d97455acab9bf4f648bb93f0d9b
francies <quebert88@gmail.com>
2023-06-26 12:55:45 +0100

    adding dynamic meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,14 @@ import Nav from '@/components/Nav'
 import Provider from '@/components/Provider'
 import '@/styles/global.css'
 
-// export const metadata = {
-//     title: 'Next App Promtopia',
-//     description: 'Discover and share AI prompt '
-// }
+
+export function generateMetadata() {
+    return {
+        title: 'Next App Promtopia',
+        description: 'Discover and share AI prompt '
+    };
+  }
+
 
 type Props = {
     children: React.ReactNode
